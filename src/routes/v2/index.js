a19a641b12243f28f8e7803dfc869197fdd5e722
goutamthakur <goutamthakur2508@gmail.com>
2025-06-01 11:48:51 +0530

    Removed the v2 routes

--- src/routes/v2/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { InfoController } = require("../../controllers");
-
-router.get("/info", InfoController.info);
-
-module.exports = router;
